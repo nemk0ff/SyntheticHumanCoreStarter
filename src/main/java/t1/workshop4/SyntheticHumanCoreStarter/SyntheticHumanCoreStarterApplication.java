00b6addfebccb08f2860997a9cd7960b85d81536
Nemkoff <nemkov_work@mail.ru>
2025-07-20 01:26:47 +0300

    init commit

---
+++ src/main/java/t1/workshop4/SyntheticHumanCoreStarter/SyntheticHumanCoreStarterApplication.java
@@ -0,0 +1,13 @@
+package t1.workshop4.SyntheticHumanCoreStarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyntheticHumanCoreStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyntheticHumanCoreStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/t1/workshop4/SyntheticHumanCoreStarter/SyntheticHumanCoreStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package t1.workshop4.SyntheticHumanCoreStarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyntheticHumanCoreStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
