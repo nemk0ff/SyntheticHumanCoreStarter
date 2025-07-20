5a481c769f4f70c0c3cd22343647b3449f54c7dc
Nemkoff <nemkov_work@mail.ru>
2025-07-20 16:10:55 +0300

    добавил систему мониторинга

--- src/main/java/t1/workshop4/SyntheticHumanCoreStarter/services/CommandProcessor.java
+++ src/main/java/t1/workshop4/SyntheticHumanCoreStarter/services/CommandProcessor.java
@@ -1,10 +1,14 @@
 package t1.workshop4.SyntheticHumanCoreStarter.services;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
 import jakarta.annotation.PostConstruct;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.stereotype.Service;
 import t1.workshop4.SyntheticHumanCoreStarter.dto.CommandDTO;
 import t1.workshop4.SyntheticHumanCoreStarter.dto.CommandMapper;
@@ -14,6 +18,9 @@ import t1.workshop4.SyntheticHumanCoreStarter.model.CommandPriority;
 
 @Service
 public class CommandProcessor {
+  private final AtomicInteger queueSize = new AtomicInteger();
+  private final Counter commonCommandsCounter = Metrics.counter("command.common");
+  private final Counter criticalCommandsCounter = Metrics.counter("command.critical");
   private static final Logger logger = LoggerFactory.getLogger(CommandProcessor.class);
   private final BlockingQueue<Command> commandQueue = new ArrayBlockingQueue<>(10);
 
@@ -24,11 +31,15 @@ public class CommandProcessor {
 
   public void addCommand(CommandDTO commandDto) {
     Command command = CommandMapper.INSTANCE.toCommand(commandDto);
-    
+
     if (command.getPriority() == CommandPriority.CRITICAL) {
+      criticalCommandsCounter.increment();
       executeCommandImmediately(command);
     } else {
+      commonCommandsCounter.increment();
+      queueSize.incrementAndGet();
       if (!commandQueue.offer(command)) {
+        queueSize.decrementAndGet();
         throw new CommandQueueFullException("Command queue is full");
       }
     }
@@ -39,6 +50,7 @@ public class CommandProcessor {
       try {
         Command command = commandQueue.take();
         executeCommand(command);
+        queueSize.decrementAndGet();
       } catch (InterruptedException e) {
         Thread.currentThread().interrupt();
         break;
@@ -57,5 +69,10 @@ public class CommandProcessor {
 
   private void executeCommandImmediately(Command command) {
     logger.info("Executing CRITICAL command immediately: {}", command);
+  }
+
+  @ReadOperation
+  public int getQueueSize() {
+    return queueSize.get();
   }
 }
