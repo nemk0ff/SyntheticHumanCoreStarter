053adb01e971ea77e3c6601c5e1fbeda55b43bb3
Nemkoff <nemkov_work@mail.ru>
2025-07-20 17:28:35 +0300

    поправил контейнеризацию prometheus

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,12 @@
 # Этап сборки
 FROM eclipse-temurin:21-jdk-jammy as builder
 WORKDIR /app
-COPY .mvn/ .mvn
-COPY mvnw pom.xml ./
-RUN ./mvnw dependency:go-offline
-COPY src ./src
+COPY . .
 RUN ./mvnw clean package -DskipTests
 
-# Финальный образ
-FROM eclipse-temurin:21-jre-jammy
-WORKDIR /app
-COPY --from=builder /app/target/*.jar app.jar
+# Финальный образ с Tomcat
+FROM tomcat:10.1-jdk21-temurin-jammy
+WORKDIR /usr/local/tomcat/webapps
+COPY --from=builder /app/target/*.war ROOT.war
 EXPOSE 8080
-ENTRYPOINT ["java", "-jar", "app.jar"]
+CMD ["catalina.sh", "run"]
--- src/main/java/t1/workshop4/SyntheticHumanCoreStarter/SyntheticHumanCoreStarterApplication.java
+++ src/main/java/t1/workshop4/SyntheticHumanCoreStarter/SyntheticHumanCoreStarterApplication.java
@@ -2,9 +2,15 @@ package t1.workshop4.SyntheticHumanCoreStarter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SyntheticHumanCoreStarterApplication {
+public class SyntheticHumanCoreStarterApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SyntheticHumanCoreStarterApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SyntheticHumanCoreStarterApplication.class, args);
